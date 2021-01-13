040167572499b2c8bc59266f3e7fa8c711587096
Dawid Lipiński <dlipinski@arex.local>
2021-01-13 19:18:28 +0100

    181. Sending a POST Request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -72,7 +73,24 @@ class BurgerBuilder extends Component {
 	};
 
 	purchaseContineHandler = () => {
-		alert('You continue!');
+		const order = {
+			ingredients: this.state.ingredients,
+			price: this.state.totalPrice,
+			customer: {
+				name: 'Dawid Lipiński',
+				address: {
+					street: 'Test street 1',
+					zipCode: '41351',
+					country: 'Poland',
+				},
+				email: 'test@test.com',
+			},
+			deliveryMethod: 'fastest',
+		};
+		axios
+			.post('/orders.json', order)
+			.then((response) => console.log(response))
+			.catch((error) => console.log(error));
 	};
 
 	render() {
@@ -89,7 +107,7 @@ class BurgerBuilder extends Component {
 						ingredients={this.state.ingredients}
 						purchaseCancelled={this.purchaseCancelHandler}
 						purchaseContinued={this.purchaseContineHandler}
-            price={this.state.totalPrice}
+						price={this.state.totalPrice}
 					/>
 				</Modal>
 				<Burger ingredients={this.state.ingredients} />
