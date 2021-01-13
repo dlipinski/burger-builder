0087d1cb0798cd2eed756cf3acb0a918ea957d5b
Dawid Lipiński <dlipinski@arex.local>
2021-01-13 17:26:59 +0100

    143. Adding a Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import classes from './Layout.module.scss';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
   <React.Fragment>
-    <div>Toolbar, SideDrawe, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>
       {props.children}
     </main>
