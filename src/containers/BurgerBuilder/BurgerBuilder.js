87e99c88cfffe468f2aaf26a3b6ec376efbcfa4b
Dawid Lipiński <dlipinski@arex.local>
2021-01-14 18:01:13 +0100

    304. Fetching Ingredients Asynchronously

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,27 +7,13 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import axios from '../../axios-orders';
 import * as burgerBuilderActions from '../../store/actions/index';
-
+import axios from '../../axios-orders';
 
 class BurgerBuilder extends Component {
 	state = {
-		purchasing: false,
-		loading: false,
-		error: false,
+		purchasing: false
 	};
-
-	componentDidMount() {
-		// axios
-		// 	.get('/ingredients.json')
-		// 	.then((response) => {
-		// 		this.setState({ ingredients: response.data });
-		// 	})
-		// 	.catch((error) => {
-		// 		this.setState({error: true})
-		// 	});
-	}
 
 	updatePurchaseState(ingredients) {
 		const sum = Object.values(ingredients).reduce((sum, el) => {
@@ -79,9 +65,6 @@ class BurgerBuilder extends Component {
 					/>
 				</React.Fragment>
 			);
-		}
-		if (this.state.loading) {
-			orderSummary = <Spinner />;
 		}
 		return (
 			<React.Fragment>
