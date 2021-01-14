24a5abdd78f45ff0a77eef8fc9231658b9987189
Dawid Lipiński <dlipinski@arex.local>
2021-01-14 19:10:07 +0100

    314. Resetting the Price after Purchases

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -36,7 +36,8 @@ const reducer = (state = initialState, action) => {
 		case actionTypes.SET_INGREDIENTS:
 			return {
 				...state,
-				ingredients: action.ingredients,
+        ingredients: action.ingredients,
+        totalPrice: 4,
 				error: false,
 			};
 		case actionTypes.FETCH_INGREDIENTS_FAILED:
