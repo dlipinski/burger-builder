23b9c1db751d868855737698fd676af8e3890861
Dawid Lipiński <dlipinski@arex.local>
2021-01-14 19:57:53 +0100

    318. Refactoring Reducers Continued

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -13,40 +13,51 @@ const initialState = {
 	error: false,
 };
 
+const addIngredient = (state, action) => {
+	return {
+		...state,
+		ingredients: {
+			...state.ingredients,
+			[action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+		},
+		totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+	};
+};
+
+const removeIngredient = (state, action) => {
+	return {
+		...state,
+		ingredients: {
+			...state.ingredients,
+			[action.ingredientName]: state.ingredients[action.ingredientName] - 1,
+		},
+		totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
+	};
+};
+
+const setIngredients = (state, action) => {
+	return {
+		...state,
+		ingredients: action.ingredients,
+		totalPrice: 4,
+		error: false,
+	};
+};
+
+const fetchIngerdientsFailed = (state, action) => {
+	return {
+		...state,
+		error: true,
+	};
+};
+
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
-		case actionTypes.ADD_INGREDIENT:
-			return {
-				...state,
-				ingredients: {
-					...state.ingredients,
-					[action.ingredientName]: state.ingredients[action.ingredientName] + 1,
-				},
-				totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
-			};
-		case actionTypes.REMOVE_INGREDIENT:
-			return {
-				...state,
-				ingredients: {
-					...state.ingredients,
-					[action.ingredientName]: state.ingredients[action.ingredientName] - 1,
-				},
-				totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
-			};
-		case actionTypes.SET_INGREDIENTS:
-			return {
-				...state,
-        ingredients: action.ingredients,
-        totalPrice: 4,
-				error: false,
-			};
-		case actionTypes.FETCH_INGREDIENTS_FAILED:
-			return {
-				...state,
-				error: true,
-			};
-		default:
-			return state;
+		case actionTypes.ADD_INGREDIENT: return addIngredient(state, action);
+		case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state, action);
+		case actionTypes.SET_INGREDIENTS: return setIngredients(state, action);
+		case actionTypes.FETCH_INGREDIENTS_FAILED: return fetchIngerdientsFailed(state, action);
+		default: return state;
 	}
 };
 
