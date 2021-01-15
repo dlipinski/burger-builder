8633c8414e095dd176472ec3d7b0ac6621ea3eb9
Dawid Lipiński <dlipinski@arex.local>
2021-01-15 16:50:40 +0100

    !!! Broken, but going home, wanna fix at weekend !!!

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -44,6 +44,12 @@ class Auth extends Component {
 		formIsValid: false,
 	};
 
+	componentDidMount() {
+		if (!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
+			this.props.onSetAuthRedirectPath();
+		}
+	}
+
 	checkValidity = (value, rules) => {
 		let isValid = true;
 
@@ -128,7 +134,7 @@ class Auth extends Component {
 
 		let authRedirect = null;
 		if (this.props.isAuthenticated) {
-			authRedirect = <Redirect to='/' />;
+			authRedirect = <Redirect to={this.props.authRedirectpath} />;
 		}
 
 		return (
@@ -154,12 +160,15 @@ const mapStateToProps = (state) => {
 		loading: state.auth.loading,
 		error: state.auth.error,
 		isAuthenticated: state.auth.token !== null,
+		buildingBurger: state.burgerBuilder.building,
+		authRedirectPath: state.auth.authRedirectPath
 	};
 };
 
 const mapDispatchToProps = (dispatch) => {
 	return {
 		onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp)),
+		onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
 	};
 };
 
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,55 +1,69 @@
 import * as actionTypes from '../actions/actionsTypes';
 
 const initialState = {
-  token: null,
-  userId: null,
-  error: null,
-  loading: false
+	token: null,
+	userId: null,
+	error: null,
+	loading: false,
+	authRedirectPath: '/',
 };
 
 const authStart = (state, action) => {
-  return {
-    ...state,
-    error: null,
-    loading: true
-  }
-}
+	return {
+		...state,
+		error: null,
+		loading: true,
+	};
+};
 
 const authSuccess = (state, action) => {
-  return {
-    ...state,
-    token: action.idToken,
-    userId: action.userId,
-    error: null,
-    loading: false
-  }
-}
+	return {
+		...state,
+		token: action.idToken,
+		userId: action.userId,
+		error: null,
+		loading: false,
+	};
+};
 
 const authFail = (state, action) => {
-  return {
-    ...state,
-    error: action.error,
-    loading: false
-  }
-}
+	return {
+		...state,
+		error: action.error,
+		loading: false,
+	};
+};
 
 const authLogout = (state, action) => {
-  return {
-    ...state,
-    idToken: null,
-    userId: null
-  }
-}
+	return {
+		...state,
+		idToken: null,
+		userId: null,
+	};
+};
+
+const setAuthRedirectPath = (state, action) => {
+	return {
+		...state,
+		authRedirectPath: action.path,
+	};
+};
 
 const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case actionTypes.AUTH_START: return authStart(state, action)
-    case actionTypes.AUTH_SUCCESS: return authSuccess(state, action)
-    case actionTypes.AUTH_FAIL: return authFail(state, action)
-    case actionTypes.AUTH_LOGOUT: return authLogout(state, action)
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case actionTypes.AUTH_START:
+			return authStart(state, action);
+		case actionTypes.AUTH_SUCCESS:
+			return authSuccess(state, action);
+		case actionTypes.AUTH_FAIL:
+			return authFail(state, action);
+		case actionTypes.AUTH_LOGOUT:
+			return authLogout(state, action);
+		case actionTypes.SET_AUTH_REDIRECT_PATH:
+			return setAuthRedirectPath(state, action);
+		default:
+			return state;
+	}
 };
 
 export default reducer;
