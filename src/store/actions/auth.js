7612b21419a5ee3e9011d9727448cabc68cc1da7
Dawid Lipiński <dlipinski@arex.local>
2021-01-18 13:00:42 +0100

    336. Persistent Auth State with localStorage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -24,10 +24,13 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
+	localStorage.removeItem('token');
+	localStorage.removeItem('expirationDate');
+	localStorage.removeItem('userId');
 	return {
-    type: actionTypes.AUTH_LOGOUT,
-    token: null,
-    userId: null
+		type: actionTypes.AUTH_LOGOUT,
+		token: null,
+		userId: null,
 	};
 };
 
@@ -56,6 +59,10 @@ export const auth = (email, password, isSignUp) => {
 		axios
 			.post(url, authData)
 			.then((response) => {
+				const exporationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+				localStorage.setItem('token', response.data.idToken);
+				localStorage.setItem('expirationDate', exporationDate);
+				localStorage.setItem('userId', response.data.localId);
 				dispatch(authSuccess(response.data.idToken, response.data.localId));
 				dispatch(checkAuthTimeout(response.data.expiresIn));
 			})
@@ -66,10 +73,27 @@ export const auth = (email, password, isSignUp) => {
 	};
 };
 
-
 export const setAuthRedirectPath = (path) => {
 	return {
 		type: actionTypes.SET_AUTH_REDIRECT_PATH,
-		path: path
-	}
-}
+		path: path,
+	};
+};
+
+export const authCheckState = () => {
+	return (dispatch) => {
+		const token = localStorage.getItem('token');
+		if (!token) {
+			dispatch(logout());
+		} else {
+			const expirationDate = new Date(localStorage.getItem('expirationDate'));
+			if (expirationDate > new Date()) {
+				dispatch(logout());
+			} else {
+				const userId = localStorage.getItem('userId');
+				dispatch(authSuccess(token, userId));
+				dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()));
+			}
+		}
+	};
+};
